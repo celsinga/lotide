0a28d0cf87970a75e392368cfd2705ef9071e75f
Colin <colinelsinga@gmail.com>
2020-11-25 22:23:17 +0000

    countOnly assignment

---
+++ countOnly.js
@@ -0,0 +1,50 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+    console.log(item);
+  }
+
+  return results;
+};
+
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(firstNames["Jason"], 1);
+assertEqual(firstNames["Karima"], undefined);
+assertEqual(firstNames["Fang"], 2);
+assertEqual(firstNames["Agouhanna"], undefined);
