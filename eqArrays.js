c739beaeded1055f7267a6a6bfccb4bf3668960c
Colin <colinelsinga@gmail.com>
2020-12-01 00:35:50 +0000

    new refactors

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,7 @@
+const assertEqual = require('./assertEqual');
+
 const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne !== arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
     return false;
   } else {
     for (let i = 0; i < arrayOne.length; i++) {
@@ -11,9 +13,4 @@ const eqArrays = function(arrayOne, arrayTwo) {
   }
 };
 
-
-eqArrays([1, 2, 3], [1, 2, 3]), true; // => should PASS
-eqArrays([1, 2, 3], [3, 2, 1]), false; // => should PASS
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]), true;
-eqArrays(["1", "2", "3"], ["1", "2", 3]), false;
+module.exports = eqArrays;
